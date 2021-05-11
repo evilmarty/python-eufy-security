373f4bbb90c79c6f6b1fc678bcb7e5b297cdae45
Marty Zalega <marty@zalega.me>
2021-05-12 06:50:47 +1000

    Fix up DeviceDict tests

--- tests/test_device.py
+++ tests/test_device.py
@@ -5,6 +5,13 @@ import pytest
 from eufy_security.device import Device, DeviceDict
 
 
+def test_device_update_with_dict():
+    device = Device(None, {"device_name": "device1"})
+    assert device.name == "device1"
+    device.update({"device_name": "device1updated"})
+    assert device.name == "device1updated"
+
+
 def test_device_dict_init():
     dd = DeviceDict(None)
     assert dd.item_class == Device
@@ -13,17 +20,20 @@ def test_device_dict_init():
 
 def test_device_dict_update_when_new_items():
     dd = DeviceDict(None)
-    assert "abc123" not in dd
+    assert "1" not in dd
+    assert "2" not in dd
 
-    dd.update([{"device_sn": "abc123", "params": [],}])
-    assert "abc123" in dd
+    dd.update([{"device_sn": "1", "status": 1}, {"device_sn": "2", "status": 1}])
+    assert "1" in dd
+    assert "2" in dd
 
 
 def test_device_dict_update_when_existing_items():
-    device = Device(None, {"device_sn": "abc123", "status": 1,})
     dd = DeviceDict(None)
-    dd[device.serial] = device
-    assert "abc123" in dd
+    dd.update([{"device_sn": "1", "status": 1}, {"device_sn": "2", "status": 1}])
+    assert dd["1"].status == 1
+    assert dd["2"].status == 1
 
-    dd.update([{"device_sn": "abc123", "status": 0,}])
-    assert device.status == 0
+    dd.update([{"device_sn": "1", "status": 0,}])
+    assert dd["1"].status == 0
+    assert dd["2"].status == 1
